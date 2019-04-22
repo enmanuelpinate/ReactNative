118221d910d28925227a04a72130caeda35d3868
enmanuelpinate <enmanuelpinate@gmail.com>
2019-04-22 18:05:00 +0700

    1.7.3 commit

--- src/components/SelectACity.js
+++ src/components/SelectACity.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-nati
 import Header from './Header';
 import SearchInput, { createFilter } from 'react-native-search-filter';
 import cityList from './cityList/cityList.json';
+import IconArrow from 'react-native-vector-icons/dist/Ionicons';
+import IconClear from 'react-native-vector-icons/dist/Ionicons';
 const KEYS_TO_FILTERS = ['name'];
 
 export default class SelectACity extends Component {
@@ -29,18 +31,22 @@ export default class SelectACity extends Component {
                 <Header headerText={'Select a city'} icon3={'ios-arrow-back'} style={{fontSize: 25}} 
                 goBack={() => navigate('Settings')}/>
                 <View style={styles.container}>
-                    <SearchInput 
-                    onChangeText={(term) => { this.searchUpdated(term) }} 
-                    style={styles.searchInput}
-                    placeholder="Type to search..."
-                    />
-                    <ScrollView>
+                    <View style={styles.searchContainer}>
+                        <SearchInput 
+                        onChangeText={(term) => { this.searchUpdated(term) }} 
+                        style={styles.searchInput}
+                        placeholder="Search"
+                        />
+                        <IconClear name={'ios-close'} style={styles.iconCloseStyle}/>
+                    </View>
+                    <ScrollView style={styles.cityContainer}>
                         {filteredCities.map(cityList => {
                             return (
                             <TouchableOpacity key={cityList.id.toString()} onPress={() => navigate('Home', {
                                 selectedCity: cityList.id
                             })} style={styles.cityItem}>
                                 <Text>{cityList.name}, {cityList.country}</Text>
+                                <IconArrow name={'ios-arrow-forward'} style={styles.iconStyle}/>
                             </TouchableOpacity>
                             )
                         })}
@@ -57,14 +63,33 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       justifyContent: 'flex-start'
     },
+    searchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderColor: '#CCC',
+        borderBottomWidth: 1,
+    },
     cityItem:{
       borderBottomWidth: 0.5,
       borderColor: 'rgba(0,0,0,0.3)',
-      padding: 10
+      padding: 12,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      fontSize: 20
     },
     searchInput:{
       padding: 10,
-      borderColor: '#CCC',
-      borderWidth: 1
+      fontSize: 18,
+      width: 380
+    },
+    iconStyle: {
+        fontSize: 20
+    },
+    iconCloseStyle: {
+        fontSize: 30
+    },
+    cityContainer: {
+        paddingRight: 15,
+        paddingLeft: 15
     }
   });
