cdee9aa02e99a402b9a13bfc24833c81dd031940
enmanuelpinate <enmanuelpinate@gmail.com>
2019-04-23 15:37:17 +0700

    1.7.4 commit

--- src/components/SelectACity.js
+++ src/components/SelectACity.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import Header from './Header';
 import SearchInput, { createFilter } from 'react-native-search-filter';
 import cityList from './cityList/cityList.json';
@@ -15,18 +15,35 @@ export default class SelectACity extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          searchTerm: ''
+          searchTerm: '',
+          closeIconDisplay: false,
+          loading: true,
+          cities: []
         }
       }
 
+    componentWillMount() {
+        this.setState({ cities: cityList, loading: false })
+    }
+
     searchUpdated(term) {
         this.setState({ searchTerm: term })
+        term === '' ? this.setState({ closeIconDisplay: false })
+        : this.setState({ closeIconDisplay: true })
     }
 
     render() {
+        const data = this.state.cities;
         const { navigate } = this.props.navigation;
-        const filteredCities = cityList.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
-        return (
+        const filteredCities = data.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
+        const display = this.state.closeIconDisplay ? "flex" : "none";
+        return ( this.state.loading ? 
+            <View>
+                <Header headerText={'Select a city'} icon3={'ios-arrow-back'} style={{fontSize: 25}} 
+                goBack={() => navigate('Settings')}/>
+                <ActivityIndicator size="large" color="#1CC1FD" />
+            </View>
+            :
             <View style={{ flex: 1 }}>
                 <Header headerText={'Select a city'} icon3={'ios-arrow-back'} style={{fontSize: 25}} 
                 goBack={() => navigate('Settings')}/>
@@ -37,15 +54,17 @@ export default class SelectACity extends Component {
                         style={styles.searchInput}
                         placeholder="Search"
                         />
-                        <IconClear name={'ios-close'} style={styles.iconCloseStyle}/>
+                        <TouchableOpacity>
+                            <IconClear name={'ios-close'} style={[styles.iconCloseStyle, {display}]}/>
+                        </TouchableOpacity>
                     </View>
                     <ScrollView style={styles.cityContainer}>
-                        {filteredCities.map(cityList => {
+                        {filteredCities.map(data => {
                             return (
-                            <TouchableOpacity key={cityList.id.toString()} onPress={() => navigate('Home', {
-                                selectedCity: cityList.id
+                            <TouchableOpacity key={data.id.toString()} onPress={() => navigate('Home', {
+                                selectedCity: data.id
                             })} style={styles.cityItem}>
-                                <Text>{cityList.name}, {cityList.country}</Text>
+                                <Text>{data.name}, {data.country}</Text>
                                 <IconArrow name={'ios-arrow-forward'} style={styles.iconStyle}/>
                             </TouchableOpacity>
                             )
@@ -80,13 +99,15 @@ const styles = StyleSheet.create({
     searchInput:{
       padding: 10,
       fontSize: 18,
-      width: 380
+      width: 360
     },
     iconStyle: {
         fontSize: 20
     },
     iconCloseStyle: {
-        fontSize: 30
+        fontSize: 35,
+        padding: 10,
+        paddingRight: 30
     },
     cityContainer: {
         paddingRight: 15,
